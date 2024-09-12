0df7b23e115d48c03c403a1bf00f37073f901e2e
Aimé Lovasoa <louvashu@gmail.com>
2024-09-12 09:28:45 +0300

    Initialisation

---
+++ resources/views/nova/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="text-center">
+    <p>
+        This Product Analytics Dashboard has been powered by
+        <a
+            href="https://blog.code.com"
+            class="text-yellow-600 hover:text-yellow-500 transition-all font-bold">
+            Code with MLS
+        </a>
+        . Feel free to support him.
+    </p>
+</div>
